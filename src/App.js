46e77338db3bad270ab64334bfe51851605d1228
Bregnballe <bregnballe@gmail.com>
2021-01-15 15:09:42 +0100

    Abortcontrollers, remaining results state

--- src/App.js
+++ src/App.js
@@ -11,48 +11,68 @@ import {List} from "./components/List"
 
 export const App = () => {
     const [data, setData] = useState([]);
+    const [total, setTotal] = useState("")
+    const [remaining, setRemaining] = useState("")
     const [searchTerm, setSearchTerm] = useState("");
     //const [isSearching, setIsSearching] = useState(false);
     const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
-    const limit = 100;
-    const controller = new AbortController();
-
-//TEST
+    const limit = 50;
+    const initialController = new AbortController();
+    const additionalController = new AbortController();
 
     // GENERAL FETCH API DATA
-    const getData = async (queryString, controller) => {
+    const getInitialData = async (queryString, controller) => {
+        additionalController.abort();
+        const response = await fetch(`http://localhost:5000/names?limit=${limit}${queryString}`,{signal: controller.signal});
+        const responseData = await response.json();
+        return responseData
+    }
+
+    const getAdditionalData = async (queryString, controller) => {
+        initialController.abort();
         const response = await fetch(`http://localhost:5000/names?limit=${limit}${queryString}`,{signal: controller.signal});
         const responseData = await response.json();
         return responseData
     }
 
 
+// load initial data
+// scroll -> fetch -> cancelled
+// search -> fetch
+// scroll -> won't fetch (Maybe abortcontrollers`?)
+
+
     //FETCHING ADDITIONAL DATA ON SCROLL
     const fetchAdditionalData = async () => {
+        initialController.abort();
         
+        if (remaining > 0) {
         const queryString = debouncedSearchTerm ? `&initial=false&search=${debouncedSearchTerm}` : '&initial=false';
-        console.log("fetching additional data");
 
-        const additionalData = await getData(queryString, controller)
-        setData([...data, ...additionalData.names]);
+        const additionalData = await getAdditionalData(queryString, additionalController)
+        additionalData.names ? setData([...data, ...additionalData.names]) : setData(data);
+        additionalData.total ? setRemaining(additionalData.total-additionalData.names.length) : setRemaining(remaining);
+    }
     };
 
     //FETCHING INITIAL DATA ON FIRST RENDER OR SEARCH
     useEffect(() => {
         let mounted = true;
-        
 
         const fetchInitialData = async () => {
 
             if (!mounted) return setData([]);
             
-            setData([]);
+            //setData([]);
+            additionalController.abort();
 
             const queryString = debouncedSearchTerm ? `&initial=true&search=${debouncedSearchTerm}` : '&initial=true';
-            console.log("fetching initial data");
-            const initialData = await getData(queryString, controller)
-            setData(initialData.names);
+
+            const initialData = await getInitialData(queryString, initialController)
+            initialData.names ? setData(initialData.names) : setData([]) ;
+            initialData.total ? setTotal(initialData.total) : setTotal("0");
+            initialData.total ? setRemaining(initialData.total-initialData.names.length) : setRemaining("0");
         
         };
 
@@ -61,7 +81,7 @@ export const App = () => {
 
         return () => {
             mounted = false;
-            controller.abort();
+            initialController.abort();
         }
         
     }, [debouncedSearchTerm]);
@@ -70,6 +90,8 @@ export const App = () => {
 
     // HANDLE SEARCH INPUT CHANGE
     const handleChange = (e) => {
+        initialController.abort();
+        additionalController.abort();
         setSearchTerm(e.currentTarget.value);
     }
 
@@ -88,6 +110,12 @@ export const App = () => {
             value={searchTerm}
             placeholder="Search"
         />
+        <p
+        style={{position: "fixed", top: "12px"}}
+        >Total: {total}</p> 
+        <p
+        style={{position: "fixed", top: "36px"}}
+        >Remaining: {remaining}</p> 
         <InfiniteScroll
             dataLength={data.length}
             next={fetchAdditionalData}
